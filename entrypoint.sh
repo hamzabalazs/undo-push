58b211aabc1839d3f3af59f8c489534f98643649
hamzabalazs <79209379+hamzabalazs@users.noreply.github.com>
2023-04-27 10:50:58 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,6 @@
 
 cd "${GITHUB_WORKSPACE}"
 
-git branch ${GITHUB_REF}
 git config --global user.name "github-actions[bot]"
 git config --global user.email "github-actions[bot]@users.noreply.github.com"
 git reset --hard HEAD~1
