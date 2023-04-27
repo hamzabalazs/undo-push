768791ff082889f2fe751a38af67fadfd80707aa
hamzabalazs <79209379+hamzabalazs@users.noreply.github.com>
2023-04-27 11:09:46 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,4 @@ git branch refs/heads/main
 git config --global user.name "github-actions[bot]"
 git config --global user.email "github-actions[bot]@users.noreply.github.com"
 git reset --hard HEAD~1
-git push --force origin refs/heads/main
+git push --force origin HEAD:refs/heads/main
